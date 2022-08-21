497428d98c505faa4c8447679c4d5f3ccd223aa6
admiration91 <almaslov91@gmail.com>
2022-08-21 11:45:38 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def index(request):
+    return HttpResponse('Главная страница тут будет')
+
+def group_posts(request, pk):
+    return HttpResponse(f'Пост номер {pk} будет тут')
 # Create your views here.
